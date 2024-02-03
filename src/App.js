27a21c29bdd8bc0f55cfdfc915fe551adc3f8157
mogosbogdan <mogosbogdan@yahoo.com>
2024-02-03 23:18:20 +0200

    let's see if this works

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
-import { useState } from "react";
-import { Drawer } from "@mui/material";
+// import { useState } from "react";
+// import { Drawer } from "@mui/material";
 
 function App() {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
 
   return (
-   <>
-    <div className="App">
-      <button onClick={handleClick}>Click me</button>
-      <>
-        <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
-          <div style={{ height: "400px", textAlign: "center" }}>
-            <button onClick={() => setOpen(false)}>close</button>
-          </div>
-        </Drawer>
-      </>
-    </div>
-    <>see me ?</>
-   </>
+    <>
+      {/* <div className="App">
+        <button onClick={handleClick}>Click me</button>
+        <>
+          <Drawer anchor={"bottom"} open={open} onClose={() => setOpen(false)}>
+            <div style={{ height: "400px", textAlign: "center" }}>
+              <button onClick={() => setOpen(false)}>close</button>
+            </div>
+          </Drawer>
+        </>
+      </div> */}
+      <>see me ?</>
+    </>
   );
 }
 
